7c5a378b1ac97deb0f44a82aa15281f0ea3e9047
Spencer Sharp <spencersharp@me.com>
2017-10-19 21:23:14 -0500

    Moved testing code and implements block to lib.rs

--- src/obj.rs
+++ src/obj.rs
@@ -53,32 +53,3 @@ pub struct PathFinder {
 	pub wp_list: Vec<Waypoint>,
 	pub flyzone_points: Vec<Point>,
 }
-impl PathFinder
-{
-  //TODO
-  fn adjust_path(self) -> Vec<Waypoint> {
-    self.wp_list.clone()
-  }
-  
-  fn set_x(&mut self, new_x: u32){
-    self.delta_x = new_x;
-  }
-
-  fn set_waypoint_list(&mut self, list: Vec<Waypoint>) {
-    self.wp_list = list;
-  }
-}
-
-fn main()
-{
-  let mut point : Point = Point{lat: 0.0, lon: 0.0};
-  let mut point2 : Point = Point{lat: 0.0, lon: 0.0};
-  let mut point3 : Point = Point{lat: 0.0, lon: 0.0};
-  let mut vec : Vec<Waypoint> = vec![Waypoint{index: 0, coords: point, alt: 0.0, radius: 0.0}];
-  let mut obstacles : Vec<Obstacle> = vec![Obstacle{coords: point2, radius: 0.0, height: 0.0}];
-  let mut flyzones : Vec<Point> = vec![point3];
-  let mut plane : Plane = Plane::new(0.0,0.0,0.0);
-  //let mut vec2 : Vec<Waypoint> = vec![Waypoint{}];
-  let mut f : PathFinder = PathFinder{delta_x: 0, buffer: 0, max_process_time: 0, plane: plane,obstacle_list: obstacles, wp_list : vec, flyzone_points : flyzones};
-  f.set_x(5);
-}
