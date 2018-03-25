9d5d1d8556fe3abe54431092c1b60f61ec544db9
steven <stevenzhu@utexas.edu>
2018-03-25 18:11:59 -0500

    Updated main to work with new library (because apparently cargo wouldn't use a commit if it fails travis)

--- src/main.rs
+++ src/main.rs
@@ -13,30 +13,38 @@ fn main() {
         Point::from_degrees(30.32247, -97.60325),
         Point::from_degrees(30.32473, -97.6041)
     );
+    println!("Flightzone:");
+    for point in &flight_zone {
+        println!("{:.5}, {:.5}", point.lat_degree(), point.lon_degree());
+    }
+    println!();
     let obstacles = vec!(
-        // Obstacle{coords: Point::from_degrees(30.32456, -97.60283), radius: 120.0, height: 1.0},
-        Obstacle{coords: Point::from_degrees(30.32447, -97.60157), radius: 120.0, height: 1.0},
-        Obstacle{coords: Point::from_degrees(30.32374, -97.60232), radius: 120.0, height: 1.0}
+        Obstacle{coords: Point::from_degrees(30.32456, -97.60283), radius: 50.0, height: 1.0},
+        Obstacle{coords: Point::from_degrees(30.32447, -97.60157), radius: 50.0, height: 1.0},
+        Obstacle{coords: Point::from_degrees(30.32374, -97.60232), radius: 50.0, height: 1.0}
     );
     let waypoints = vec!(
         Waypoint::new(Point::from_degrees(30.32392, -97.60157))
     );
-    let mut path_finder1 = PathFinder::new(10.0, flight_zone);
+    let flyzone = vec!(flight_zone);
+    let mut path_finder1 = PathFinder::new(10.0, flyzone);
     path_finder1.set_obstacle_list(obstacles);
     path_finder1.set_waypoint_list(waypoints);
     let mut result = path_finder1.adjust_path(Plane::new(30.32456, -97.60283, 10.0));
     if let Some(result) = result {
+        println!("A* Result");
         for node in result {
             println!("{:.5}, {:.5}", node.location.lat_degree(), node.location.lon_degree());
         }
     }
-    // result = path_finder1.adjust_path_jump_point(Plane::new(30.32456, -97.60283, 10.0));
-    // if let Some(result) = result {
-    //     for node in result {
-    //         println!("{:.5}, {:.5}", node.location.lat_degree(), node.location.lon_degree());
-    //     }
-    // }
-    // for node in path_finder1.obstacle_list {
-    //     println!("x: {} y: {}", node.x, node.y);
-    // }
+    println!();
+
+    // path_finder1.draw(0,100,0,100);
+    result = path_finder1.adjust_path_jump_point(Plane::new(30.32456, -97.60283, 10.0));
+    if let Some(result) = result {
+        println!("Jump Result");
+        for node in result {
+            println!("{:.5}, {:.5}", node.location.lat_degree(), node.location.lon_degree());
+        }
+    }
 }
