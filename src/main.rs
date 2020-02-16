47e986db11535a8e47bf3727c0f8c4dbb12f3216
adampeninger <adam.peninger@gmail.com>
2020-02-16 17:38:29 -0600

    Made fn main() to run pathfinder and further segmented skeleton
    The parsers in process.rs still need to be defined. Lots of unit testing
    is needed.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,55 @@
 extern crate pathfinder;
 
 use pathfinder::*;
+use std::collections::LinkedList;
 use std::io;
-use std::io::Read;
+use std::io::{BufReader, Read};
 
-pub fn main() {
+pub fn main() -> io::Result<()> {
+    // read to end of incoming data (should include everything for Process)
+    let mut reader = BufReader::new(io::stdin());
     let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer);
-    let process = Process::parse(&buffer);
+    reader.read_to_string(&mut buffer)?;
+    // create a Process and insure it is valid (None means it was invalid)
+    let process: Process;
+    match Process::parse(buffer) {
+        Some(p) => process = p,
+        None => return Err(io::Error::from(io::ErrorKind::InvalidData)),
+    }
+
+    let plane = Plane::new(process.plane_location);
+    let mut pathfinder = Pathfinder::new(
+        Tanstar::new(),
+        process.config,
+        process.flyzones,
+        process.obstacles,
+    );
+    let result = pathfinder.get_adjust_path(plane, process.waypoints);
+    output_result(result, plane);
+
+    Ok(())
+}
+
+fn output_result<T>(result: LinkedList<Waypoint<T>>, plane: Plane) {
+    // send plane position
+    println!(
+        "\nPlane: {},{},{}",
+        plane.location.lat_degree(),
+        plane.location.lon_degree(),
+        plane.location.alt()
+    );
+
+    // send waypoint positions (waypoints separated by spaces)
+    print!("Waypoints: ");
+    let mut iter = result.iter();
+    while let Some(node) = iter.next() {
+        print!(
+            " {},{},{}",
+            node.location.lat_degree(),
+            node.location.lon_degree(),
+            node.location.alt
+        );
+    }
+
+    println!();
 }
--- src/process/process.rs
+++ src/process/process.rs
@@ -37,22 +37,99 @@ impl Process {
         pathfinder.get_adjust_path(plane.clone(), self.waypoints.clone())
     }
 
-    pub fn parse(input: &String) -> Self {
+    pub fn parse(input: String) -> Option<Self> {
+        let mut config: Option<TConfig> = None;
+        let mut flyzones: Option<Vec<Vec<Location>>> = None;
+        let mut plane_location: Option<Location> = None;
+        let mut obstacles: Option<Vec<Obstacle>> = None;
+        let mut waypoints: Option<LinkedList<Waypoint<()>>> = None;
+
         // parse into data
+        let lines = input.split("\n");
+        for line in lines {
+            let data_type = &line[..line.rfind(": ")?];
+            // check what kind of data is contained in this line
+            match data_type {
+                "Config:" => config = Self::parse_config(line.to_string()),
+                "Flyzones:" => flyzones = Self::parse_flyzones(line.to_string()),
+                "Plane_Location:" => plane_location = Self::parse_plane_location(line.to_string()),
+                "Obstacles:" => obstacles = Self::parse_obstacles(line.to_string()),
+                "Waypoints:" => waypoints = Self::parse_waypoints(line.to_string()),
+                _ => (),
+            }
+        }
 
-        // let mut iter = input.split_whitespace();
-
-        // return placeholder
-        Self::new(
-            vec![vec![Location::from_degrees(0f64, 0f64, 0f32)]],
-            vec![Obstacle::new(
-                Location::from_degrees(0f64, 0f64, 0f32),
-                0f32,
-                0f32,
-            )],
-            LinkedList::new(),
-            Location::from_degrees(0f64, 0f64, 0f32),
-            TConfig::default(),
-        )
+        // return Process
+        Some(Self::new(
+            flyzones?,
+            obstacles?,
+            waypoints?,
+            plane_location?,
+            config?,
+        ))
     }
+
+    fn parse_config(input: String) -> Option<TConfig> {
+        // return placeholder
+        Some(TConfig::default())
+    }
+
+    fn parse_flyzones(input: String) -> Option<Vec<Vec<Location>>> {
+        // return placeholder
+        Some(vec![vec![Location::from_degrees(0f64, 0f64, 0f32)]])
+    }
+
+    fn parse_plane_location(input: String) -> Option<Location> {
+        // return placeholder
+        Some(Location::from_degrees(0f64, 0f64, 0f32))
+    }
+
+    fn parse_obstacles(input: String) -> Option<Vec<Obstacle>> {
+        // return placeholder
+        Some(vec![Obstacle::new(
+            Location::from_degrees(0f64, 0f64, 0f32),
+            0f32,
+            0f32,
+        )])
+    }
+
+    fn parse_waypoints(input: String) -> Option<LinkedList<Waypoint<()>>> {
+        // return placeholder
+        Some(LinkedList::new())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    /*
+    #[test]
+    fn test_parse_config() {
+
+    }
+
+    #[test]
+    fn test_parse_flyzones() {
+
+    }
+
+    #[test]
+    fn test_parse_plane_location() {
+
+    }
+
+    #[test]
+    fn test_parse_obstacles() {
+
+    }
+
+    #[test]
+    fn test_parse_waypoints() {
+
+    }
+
+    #[test]
+    fn test_parse() {
+
+    }
+    */
 }
