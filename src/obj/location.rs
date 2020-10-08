5ba60e07c29e7e6babdbc43a6b9e2eff6cdbdd99
adampeninger <adam.peninger@gmail.com>
2020-10-08 12:04:20 -0500

    Added and updated documentation comments for location.rs

--- src/obj/location.rs
+++ src/obj/location.rs
@@ -3,14 +3,19 @@ extern crate ordered_float;
 use self::ordered_float::*;
 use std::fmt;
 
+/// A struct for storing a position in world coordinates
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub struct Location {
-    lat: OrderedFloat<f64>,     //In radians
-    lon: OrderedFloat<f64>,     //In radians
-    pub alt: OrderedFloat<f32>, //In meters
+    /// Latitude in radians
+    lat: OrderedFloat<f64>,
+    /// Longitude in radians
+    lon: OrderedFloat<f64>,
+    /// Altitude in meters
+    pub alt: OrderedFloat<f32>,
 }
 
 impl fmt::Display for Location {
+    /// Formats the location for display using the given formatter
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
@@ -23,7 +28,7 @@ impl fmt::Display for Location {
 }
 
 impl Location {
-    // Create location from coordinates in degrees
+    /// Create location from coordinates in degrees
     pub fn from_degrees(lat: f64, lon: f64, alt: f32) -> Self {
         Self {
             lat: lat.to_radians().into(),
@@ -31,7 +36,7 @@ impl Location {
             alt: alt.into(),
         }
     }
-    // Create location from coordinates in radians
+    /// Create location from coordinates in radians
     pub fn from_radians(lat: f64, lon: f64, alt: f32) -> Self {
         Self {
             lat: lat.into(),
@@ -40,18 +45,23 @@ impl Location {
         }
     }
 
+    /// Get latitude in radians
     pub fn lat(&self) -> f64 {
         self.lat.into()
     }
+    /// Get longitude in radians
     pub fn lon(&self) -> f64 {
         self.lon.into()
     }
+    /// Get altitude in radians
     pub fn alt(&self) -> f32 {
         self.alt.into()
     }
+    /// Get latitude in degrees
     pub fn lat_degree(&self) -> f64 {
         Into::<f64>::into(self.lat) * 180f64 / ::std::f64::consts::PI
     }
+    /// Get longitude in degrees
     pub fn lon_degree(&self) -> f64 {
         Into::<f64>::into(self.lon) * 180f64 / ::std::f64::consts::PI
     }
